a32129b9bfa82f71b3c134972c9580788fadc0c2
Miniot BV <miniot@kuijt-evers.net>
2023-10-11 01:28:12 +0200

    arm glitch opgelost
    en wat variabel namen veranderd om te zorgen dat ik niet in de war raak

--- wheel2/serialcomm.cpp
+++ wheel2/serialcomm.cpp
@@ -97,11 +97,11 @@ void SerialComm::checkReceivedLine(String line, eCheckMode mode) {
 
   //-------------------------------------------------- ARM --------------------------------------------------
   println(mode);
-  if (checkLineCommand( "NE",     "Needle down",                mode)) { _arm.needleDown();                   return; }
-  if (checkLineCommand( "NA",     "Needle up",                  mode)) { _arm.needleUp();                     return; }
+  if (checkLineCommand( "NE",     "Needle down",                mode)) { _arm.putNeedleInGrove();                   return; }
+  if (checkLineCommand( "NA",     "Needle up",                  mode)) { _arm.dockNeedle();                     return; }
   if (checkLineFloat(   "ATG",    "Arm targetweight",           mode, _arm.targetWeight)) { _storage.saveRequired  = true; return; }
   if (checkLineFloat(   "AG",     "Arm weight",                 mode, _arm.weight)) {                         return; }
-  if (checkLineCommand( "AKHOK",  "Arm force Home calibrate",   mode)) { _arm.justHomeWeight = _arm.weight; Serial.println(padRight("AKHOK", 8) + " " + padRight("Arm force Home calibrate", 26)   + " SET: " + String(_arm.justHomeWeight, 5)); _storage.saveRequired = true; return; }
+  if (checkLineCommand( "AKHOK",  "Arm force Home calibrate",   mode)) { _arm.justDockedWeight = _arm.weight; Serial.println(padRight("AKHOK", 8) + " " + padRight("Arm force Home calibrate", 26)   + " SET: " + String(_arm.justDockedWeight, 5)); _storage.saveRequired = true; return; }
   if (checkLineCommand( "AKL",    "Arm force 500mg calibrate",  mode)) { _arm.forceLow = _arm.force;       Serial.println(padRight("AKL", 8)   + " " + padRight("Arm force 500mg calibrate", 26)  + " SET: " + String(_arm.forceLow, 5));       _storage.saveRequired = true; return;}
   if (checkLineCommand( "AKH",    "Arm force 4000mg calibrate", mode)) { _arm.forceHigh = _arm.force;      Serial.println(padRight("AKH", 8)   + " " + padRight("Arm force 4000mg calibrate", 26) + " SET: " + String(_arm.forceHigh, 5));      _storage.saveRequired = true; return;}
   if (checkLineFloat(   "AK",     "Arm force",                  mode, _arm.force)) { _arm.force = limitFloat(_arm.force, 0, 1); return;}
@@ -360,6 +360,9 @@ void SerialComm::printGraphicData() {
   Serial.print(", ");
   Serial.print(_speedcomp.trackSpacing, 3);
 
+    Serial.print(", ");
+  Serial.print(_arm.weight, 3);
+
  Serial.println();
 } // printGraphicData()
 
@@ -372,11 +375,11 @@ void SerialComm::report() {
   Serial.println(padRight("WHEEL_TEMPERATURE", padR) +      ": " + String(analogReadTemp(), 2) + " °C");
   // Serial.println(padRight("WHEEL_STATE", padR) +            ": " + getState(_shared.state));
   // Serial.println(padRight("WHEEL_VOLUME", padR) +           ": " + String(_amplifier.volume));
-  Serial.println(padRight("WHEEL_WIRELESS_VERSION", padR) + ": " + String(_bluetooth.wirelessVersion ? "YES" : "NO"));
+  // Serial.println(padRight("WHEEL_WIRELESS_VERSION", padR) + ": " + String(_bluetooth.wirelessVersion ? "YES" : "NO"));
   Serial.println();
   _storage.info();
   _orientation.info();
-  _speedcomp.info();
+  // _speedcomp.info();
   Serial.println("----------------------------------------------");
 } // report()
 
